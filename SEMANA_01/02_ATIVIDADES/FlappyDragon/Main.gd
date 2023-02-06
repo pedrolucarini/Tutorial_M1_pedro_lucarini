ac5396dcad93ee444f63c3e18c2ef012cb2a66e2
pedrolucarini <124262220+pedrolucarini@users.noreply.github.com>
2023-02-05 23:30:52 -0300

    ponderada

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 1 # variável binária que controla se o jogo está em andamento ou não
+var vscore = 0 # variável que armazena o valor da pontuação do jogador
+var x = 1.5 # variável que influencia na velocidade do background e das colunas
+var y = 1.5 # variável que determina a intensidade da gravidade que atua no dragão
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -41,11 +41,11 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 3
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 6
 			
 	elif status == 0: # parado
 		
